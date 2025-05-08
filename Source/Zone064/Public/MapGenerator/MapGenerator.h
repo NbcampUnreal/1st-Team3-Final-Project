0082e90b7c245276f805eef7e93df274eca5a0a5
cmgeugene <jscj0815@gmail.com>
2025-05-09 01:44:23 +0900

    [Update] MapGenerator 알고리즘 고도화
    MapGenerator
    - 알고리즘 수정
    - 맵 생성 규칙 추가

--- Source/Zone064/Public/MapGenerator/MapGenerator.h
+++ Source/Zone064/Public/MapGenerator/MapGenerator.h
@@ -6,6 +6,47 @@
 #include "Engine/AssetManager.h"
 #include "MapGenerator.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EZoneType : uint8
+{
+    None        UMETA(DisplayName = "None"),
+    Road        UMETA(DisplayName = "Road"),
+    HighRise    UMETA(DisplayName = "High Rise"),
+    LowRise     UMETA(DisplayName = "Low Rise"),
+    Special     UMETA(DisplayName = "Special")
+};
+
+USTRUCT(BlueprintType)
+struct FGridCellData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadWrite)
+    EZoneType ZoneType = EZoneType::None;
+
+    UPROPERTY(BlueprintReadWrite)
+    bool bIsCrossroad = false;
+
+    UPROPERTY(BlueprintReadWrite)
+    bool bUsed = false;
+
+    UPROPERTY(BlueprintReadWrite)
+    FRotator PreferredRotation = FRotator::ZeroRotator;
+};
+
+USTRUCT(BlueprintType)
+struct FZonePrefabSet
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    EZoneType ZoneType;
+
+    UPROPERTY(EditAnywhere)
+    TArray<TSoftClassPtr<AActor>> Prefabs;
+};
+
 UCLASS()
 class ZONE064_API AMapGenerator : public AActor
 {
@@ -27,25 +68,35 @@ public:
 
     // 블럭 간 간격
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation")
-    float TileSize = 1000.0f;
+    float TileSize = 500.0f;
 
-    // Soft reference 리스트
+    // 에디터에서 사용할 소프트 레퍼 리스트
     UPROPERTY(EditAnywhere, Category = "Generation")
-    TArray<TSoftClassPtr<AActor>> BlockPrefabAssets;
-
-    // 런타임 캐싱된 실제 클래스
-    TArray<TSubclassOf<AActor>> CachedPrefabs;
+    TArray<FZonePrefabSet> BlockPrefabSets;
+ 
+    TMap<EZoneType, TArray<TSoftClassPtr<AActor>>> BlockPrefabAssetsByZone;
+    TMap<EZoneType, TArray<TSubclassOf<AActor>>> CachedPrefabsByZone;
 
     // 비동기 로딩 후 콜백
     void OnPrefabsLoaded();
 
     // 랜덤 시드
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation")
-    int32 Seed = 42;
+    int32 Seed;
+
+    void AssignSpecialClusters();
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation|Special")
+    int32 RequiredClusterSize;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation|Special")
+    float SpecialChance;
+
 
 private:
     FRandomStream RandomStream;
     FStreamableManager AssetLoader;
+    // 구역 맵
+    TMap<FIntPoint, FGridCellData> ZoneMap;
 
     void GenerateMap();
+    void GenerateZoneMap();
 };
