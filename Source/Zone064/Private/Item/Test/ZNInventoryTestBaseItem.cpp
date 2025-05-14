c7e424fd0d328850bda0594863b8739191ed779b
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-14 12:05:35 +0900

    [Add] : 아이템 습득 로직 구현
    ZNInventoryComponent :
    - [아이템 습득] 아이템 저장 TArray 추가
      - [아이템 습득] 인벤토리에 아이템이 들어갈 충분한 공간이 있는지 확인하고, 있으면 아이템 습득
    
    ZNInventoryTestBaseItem
    - [아이템 습득] 아이템에 대한 데이터를 저장
      - [아이템 습득] Dimenisons : 인벤토리에서 아이템이 차지할 격자의 크기
    
    아이템 습득 로직 : 플레이어 캐릭터가 Item과 Overlap하면, 해당 Item Actor를 인벤토리의 타일과 1:1 대응되는 Item Array에 추가

---
+++ Source/Zone064/Private/Item/Test/ZNInventoryTestBaseItem.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Item/Test/ZNInventoryTestBaseItem.h"
+
+#include "Components/SphereComponent.h"
+
+AZNInventoryTestBaseItem::AZNInventoryTestBaseItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(Mesh);
+}
+
+void AZNInventoryTestBaseItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AZNInventoryTestBaseItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FIntPoint AZNInventoryTestBaseItem::GetDimensions() const
+{
+	return Dimensions;
+}
--- Source/Zone064/Public/Character/Inventory/ZNInventoryComponent.h
+++ Source/Zone064/Public/Character/Inventory/ZNInventoryComponent.h
@@ -4,6 +4,7 @@
 #include "Components/ActorComponent.h"
 #include "ZNInventoryComponent.generated.h"
 
+class AZNInventoryTestBaseItem;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ZONE064_API UZNInventoryComponent : public UActorComponent
@@ -11,23 +12,35 @@ class ZONE064_API UZNInventoryComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Info|Inventory Colums")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Info|Inventory Columns")
 	int32 Columns;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Info|Inventory Rows")
 	int32 Rows;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Info|Inventory TileSize")
 	float TileSize;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory|Items")
+	TArray<AZNInventoryTestBaseItem*> Items;
 
 public:	
 	UZNInventoryComponent();
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable)
+	bool TryAddItem(AZNInventoryTestBaseItem* Item);
+	// TopLeftIndex : 아이템이 인벤토리에 들어갔을 때의 왼쪽 상단 Tile의 index
+	bool IsRoomAvailable(AZNInventoryTestBaseItem* Item, int32 TopLeftIndex);
+
+	FIntPoint IndexToTile(int32 Index);
+	int32     TileToIndex(FIntPoint Tile);
+	bool IsTileValid(FIntPoint Tile);
+	bool IsIndexValid(int32 Index);
+	AZNInventoryTestBaseItem* GetItemAtIndex(int32 Index);
+
+	void AddItemAt(AZNInventoryTestBaseItem* Item, int32 TopLeftIndex);
+
 protected:
 	virtual void BeginPlay() override;
-
-public:	
 	
-
-		
 };
