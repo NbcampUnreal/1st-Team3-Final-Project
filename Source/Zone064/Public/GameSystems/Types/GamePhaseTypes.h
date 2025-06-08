d330a2a2cc2b07f117e003ff71f1b828c53aa944
yoon-park <jiyoonpark.dev@gmail.com>
2025-06-09 00:56:13 +0900

    [add] Departure_N phase 추가
    - phase 추가로 인해 Lobby_GM, Gameplay_GM에서 레벨 전환 로직 변경
    - repeatcount 올려주는 위치 변경

--- Source/Zone064/Public/GameSystems/Types/GamePhaseTypes.h
+++ Source/Zone064/Public/GameSystems/Types/GamePhaseTypes.h
@@ -17,11 +17,12 @@ enum class EGamePhase : uint8
 	Title				UMETA(DisplayName = "Title"),				// 게임 시작 화면
 	Menu				UMETA(DisplayName = "Menu"),				// 세션 생성/참가 화면 (Steam 연동)
 	Lobby				UMETA(DisplayName = "Lobby"),				// 세션 대기방 (플레이어 준비, 게임 진입)
-	Departure			UMETA(DisplayName = "Departure"),			// 출발 준비 연출 (시네마틱 등)
-	Driving				UMETA(DisplayName = "Driving"),				// 목적지 이동 (이벤트 발생!)			-> 4회 반복 시작지점
+	Departure_D			UMETA(DisplayName = "Departure_D"),			// 이동 연출 오전 (시네마틱 등)			-> 4회 반복 시작지점
+	Driving				UMETA(DisplayName = "Driving"),				// 목적지 이동 (이벤트 발생!)
 	InGame				UMETA(DisplayName = "InGame"),				// 전투, 파밍
+	Departure_N			UMETA(DisplayName = "Departure_N"),			// 이동 연출 오후 (시네마틱 등)
 	Camping				UMETA(DisplayName = "Camping"),				// 정비 시간 (이벤트 발생!, 크래프팅 등)
-	Voting				UMETA(DisplayName = "Voting"),				// 다음 이동 지역 선택				-> 4회 반복 종료지점
+	Voting				UMETA(DisplayName = "Voting"),				// 다음 이동 지역 선택					-> 4회 반복 종료지점
 	Defense				UMETA(DisplayName = "Defense"),				// 최종 디펜스
 	Ending				UMETA(DisplayName = "Ending"),				// 엔딩 연출, 게임 결과 집계
 	ReturnToTitle		UMETA(DisplayName = "ReturnToTitle"),		// 세션 파괴, Title로 돌아가기
