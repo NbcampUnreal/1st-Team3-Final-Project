31569ddce1484ece1a6dddc91e88f28556a11598
yoon-park <jiyoonpark.dev@gmail.com>
2025-06-30 13:17:09 +0900

    [update] 페이즈 전환 로직 보강
    - gamephase 목록 수정
    - bp_driving_gm, bp_gameplay_gm에서의 페이즈 진입, 클리어, 전환 로직 통일
    - 특수이벤트 진입점 추가

--- Source/Zone064/Public/GameSystems/Types/GamePhaseTypes.h
+++ Source/Zone064/Public/GameSystems/Types/GamePhaseTypes.h
@@ -13,20 +13,20 @@
 UENUM(BlueprintType)
 enum class EGamePhase : uint8
 {
-	None				UMETA(DisplayName = "None"),				// 게임 시작 직후, 또는 에러 상황
-	Title				UMETA(DisplayName = "Title"),				// 게임 시작 화면
-	Menu				UMETA(DisplayName = "Menu"),				// 세션 생성/참가 화면 (Steam 연동)
-	Lobby				UMETA(DisplayName = "Lobby"),				// 세션 대기방 (플레이어 준비, 게임 진입)
-	Departure_D			UMETA(DisplayName = "Departure_D"),			// 이동 연출 오전 (시네마틱 등)			-> 4회 반복 시작지점
-	Driving				UMETA(DisplayName = "Driving"),				// 목적지 이동 (이벤트 발생!)
-	InGame				UMETA(DisplayName = "InGame"),				// 전투, 파밍
-	Departure_N			UMETA(DisplayName = "Departure_N"),			// 이동 연출 오후 (시네마틱 등)
-	Camping				UMETA(DisplayName = "Camping"),				// 정비 시간 (이벤트 발생!, 크래프팅 등)
-	Voting				UMETA(DisplayName = "Voting"),				// 다음 이동 지역 선택					-> 4회 반복 종료지점
-	Defense				UMETA(DisplayName = "Defense"),				// 최종 디펜스
-	Ending				UMETA(DisplayName = "Ending"),				// 엔딩 연출, 게임 결과 집계
-	ReturnToTitle		UMETA(DisplayName = "ReturnToTitle"),		// 세션 파괴, Title로 돌아가기
-	Test				UMETA(DisplayName = "Test"),				// 테스트용 (템플릿 전용 레벨 등)
+	None					UMETA(DisplayName = "None"),				// 게임 시작 직후, 또는 에러 상황
+	Boot					UMETA(DisplayName = "Boot"),				// 게임 시작 화면
+	Menu					UMETA(DisplayName = "Menu"),				// 세션 생성/참가 화면 (Steam 연동)
+	Lobby					UMETA(DisplayName = "Lobby"),				// 세션 대기방 (플레이어 준비, 게임 진입)
+	Departure_Day			UMETA(DisplayName = "Departure_Day"),		// 차량 이동 오전 연출 (시네마틱?)				-> 4회 반복 시작지점
+	Departure_Event			UMETA(DisplayName = "Departure_Event"),		// 차량 이동 중 일반이벤트 발생!
+	InGame					UMETA(DisplayName = "InGame"),				// 전투 지역
+	Departure_Night			UMETA(DisplayName = "Departure_Night"),		// 차량 이동 오후 연출 (시네마틱?)
+	Camping					UMETA(DisplayName = "Camping"),				// 정비 시간 (크래프팅 등)
+	Camping_Event			UMETA(DisplayName = "Camping_Event"),		// 정비 시간 중 일반이벤트 발생!				-> 4회 반복 종료지점
+	Special_Event			UMETA(DisplayName = "Special_Event"),		// 특수이벤트 발생!
+	Defense					UMETA(DisplayName = "Defense"),				// 디펜스 지역
+	Ending					UMETA(DisplayName = "Ending"),				// 엔딩 연출 (크레딧)
+	Test					UMETA(DisplayName = "Test"),				// 테스트용 (템플릿 전용 레벨?)
 };
 
 UCLASS()
